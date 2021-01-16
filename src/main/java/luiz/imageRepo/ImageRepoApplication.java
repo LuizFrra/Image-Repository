1b13b8c580daf35774494a005e5a1543bf89278c
luiz Alex <luizalex1997@gmail.com>
2021-01-16 09:44:46 -0300

    initial commit

---
+++ src/main/java/luiz/imageRepo/ImageRepoApplication.java
@@ -0,0 +1,13 @@
+package luiz.imageRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageRepoApplication.class, args);
+	}
+
+}
